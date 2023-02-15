a2fb3fefddf927c9d4bfe45aca5c973f53169f28
kevin babiarz <kevin.babiarz23@gmail.com>
2023-02-15 15:57:29 +0100

    Java Base V1

---
+++ decouverte/src/be/technifutur/decouverte/texte/Main.java
@@ -0,0 +1,13 @@
+package be.technifutur.decouverte.texte;
+
+public class Main{
+    public static void main(String[] args) {
+            String texte1 = "Bonjour"; // var texte1 :T <- "Bonjour"
+            char lettre = ' '; // var lettre :C <- ' '
+
+            System.out.println(texte1.length()); // afficher longueur(texte1)
+            System.out.println(texte1.charAt(2)); //afficher caract(texte,3)
+            System.out.println(texte1.substring(3)); // afficher 
+            System.out.println(texte1.substring(3,6)); //
+    }
+}
